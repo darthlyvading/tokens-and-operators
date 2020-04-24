fe54b586c51b6865b322ce478ccafb4a97bf71d8
Prasoon Mishra <63459571+darthlyvading@users.noreply.github.com>
2020-04-25 01:20:27 +0530

    Add files via upload

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+float a,b;
+cout<<"enter the number you want to multiply"<<endl;
+cin>>a>>b;
+cout<<"product of both the numbers is  "<<a*b<<endl;
+return 0;
+}
---
+++ sizeoff.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"sizeof int on this system is "<<sizeof(int)<<endl;
+	cout<<"sizeof char on this system is "<<sizeof(char)<<endl;
+	cout<<"sizeof float on this system is "<<sizeof(float)<<endl;
+	cout<<"sizeof double on this system is "<<sizeof(double)<<endl;
+	return 0;
+	
+}
